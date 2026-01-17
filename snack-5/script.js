b412ec73373f1c8ff02a39aacfc646e34ed0fd2d
munteanudenis87 <munteanudenis87@gmail.com>
2026-01-17 10:58:28 +0100

    risolto snack-5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,9 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+const numeriPari = nums.filter(( numero ) => {
+    if(numero % 2 === 0){
+        return true;
+    }
+});
+console.log(numeriPari);
