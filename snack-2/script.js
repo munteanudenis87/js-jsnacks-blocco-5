4a19963ce7c444d3c8d08caa26ea64c052524e43
munteanudenis87 <munteanudenis87@gmail.com>
2026-01-17 10:48:53 +0100

    risolto snack-2

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,7 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+const names = people.map(( singleName ) => {
+  return singleName.name;
+});
+console.log(names);
